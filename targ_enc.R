b94f80c8a3e01fcd83fa4023e336f0960bbc8ff8
Josh Bergstrom <jbergs12@byu.edu>
2024-10-07 23:54:17 -0600

    added eda table of counts

--- targ_enc.R
+++ targ_enc.R
@@ -10,9 +10,20 @@ amz_train <- vroom("train.csv")
 amz_test <- vroom("test.csv")
 
 # EDA
+
+table(amz_train$ROLE_DEPTNAME, amz_train$ACTION)
+
+table(amz_train$ROLE_TITLE, amz_train$ACTION)
+
 amz_train |> 
   ggplot() +
-  geom_mosaic(aes(x=product(as.factor(ROLE_DEPTNAME), fill = ACTION)))
+  geom_mosaic(aes(x=product(as.factor(ROLE_DEPTNAME),
+                            fill = as.factor(ACTION))))
+
+amz_train |> 
+  ggplot(aes(x=as.factor(RESOURCE),
+             y=as.factor(ACTION))) +
+  geom_boxplot()
 
 
 # Recipe
